45d31c65ce8aa6513e6e5afe94c3d725ecfa741b
yudaristian <yudaramasenju@gmail.com>
2024-05-08 20:26:47 +0700

    Creating Board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Board from "@/components/Board";
+import Boards from "@/components/Boards";
 import LoginView from "@/components/views/LoginView";
 import { authOptions } from "@/lib/authOptions";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -10,9 +13,14 @@ export default async function Home() {
   }
   return (
     <div>
-      <h1 className="text-4xl">Your boards :</h1>
-      boards go here....
-      {/* <Board /> */}
+      <h1 className="text-4xl mb-4">Your boards :</h1>
+      <Boards />
+      <div className="mt-4">
+        <Link className="btn primary inline-flex gap-1" href={"/new-board"}>
+          Create New Board{" "}
+          <FontAwesomeIcon className="h-6" icon={faArrowRight} />
+        </Link>
+      </div>
     </div>
   );
 }
