d862db9cc28781ddab21610286f8a26e82c4ffc2
yudaristian <yudaramasenju@gmail.com>
2024-05-13 06:28:16 +0700

    Development

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,21 +1,27 @@
-'use client';
-import {Column, useMutation, useStorage} from "@/app/liveblocks.config";
+"use client";
+import { Column, useMutation, useStorage } from "@/app/liveblocks.config";
 import NewColumnForm from "@/components/forms/NewColumnForm";
-import {LiveList, LiveObject, shallow} from "@liveblocks/core";
-import {ReactSortable} from "react-sortablejs";
-import {default as BoardColumn} from '@/components/Column';
+import { LiveList, LiveObject, shallow } from "@liveblocks/core";
+import { ReactSortable } from "react-sortablejs";
+import { default as BoardColumn } from "@/components/Column";
 
 export default function Columns() {
-  const columns = useStorage(root => root.columns.map(c => ({...c})), shallow);
+  const columns = useStorage(
+    (root) => root.columns?.map((c) => ({ ...c })),
+    shallow
+  );
 
-  const updateColumns = useMutation(({storage}, columns:LiveObject<Column>[]) => {
-    storage.set('columns', new LiveList(columns));
-  }, []);
+  const updateColumns = useMutation(
+    ({ storage }, columns: LiveObject<Column>[]) => {
+      storage.set("columns", new LiveList(columns));
+    },
+    []
+  );
 
   function setColumnsOrder(sortedColumns: Column[]) {
-    const newColumns:LiveObject<Column>[] = [];
+    const newColumns: LiveObject<Column>[] = [];
     sortedColumns.forEach((sortedColumn, newIndex) => {
-      const newSortedColumn = {...sortedColumn};
+      const newSortedColumn = { ...sortedColumn };
       newSortedColumn.index = newIndex;
       newColumns.push(new LiveObject(newSortedColumn));
     });
@@ -23,25 +29,22 @@ export default function Columns() {
   }
 
   if (!columns) {
-    return;
+    return null; // Mengembalikan null atau komponen loading sesuai kebutuhan
   }
 
   return (
     <div className="flex gap-4">
       <ReactSortable
-        group={'board-column'}
+        group={"board-column"}
         list={columns}
         className="flex gap-4"
         ghostClass="opacity-40"
-        setList={setColumnsOrder}>
-        {columns?.length > 0 && columns.map(column => (
-          <BoardColumn
-            key={column.id}
-            {...column}
-          />
-        ))}
+        setList={setColumnsOrder}
+      >
+        {columns.length > 0 &&
+          columns.map((column) => <BoardColumn key={column.id} {...column} />)}
       </ReactSortable>
-      <NewColumnForm/>
+      <NewColumnForm />
     </div>
   );
 }
