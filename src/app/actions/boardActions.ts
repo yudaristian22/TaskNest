2f7f65733d5e1871c81815c829ff59d15391745d
yudaristian <yudaramasenju@gmail.com>
2024-05-13 06:23:37 +0700

    Development

--- src/app/actions/boardActions.ts
+++ src/app/actions/boardActions.ts
@@ -1,24 +1,23 @@
-'use server';
+"use server";
 
-import {authOptions} from "@/lib/authOptions";
-import {getLiveblocksClient, liveblocksClient} from "@/lib/liveblocksClient";
-import {Liveblocks, RoomInfo} from "@liveblocks/node";
-import {getServerSession} from "next-auth";
-import uniqid from 'uniqid';
+import { authOptions } from "@/lib/authOptions";
+import { getLiveblocksClient, liveblocksClient } from "@/lib/liveblocksClient";
+import { Liveblocks, RoomInfo } from "@liveblocks/node";
+import { getServerSession } from "next-auth";
+import uniqid from "uniqid";
 
-
-export async function createBoard(name: string) : Promise<false | RoomInfo> {
+export async function createBoard(name: string): Promise<false | RoomInfo> {
   const liveblocksClient = new Liveblocks({
-    secret: process.env.LIVEBLOCKS_SECRET_KEY || '',
+    secret: process.env.LIVEBLOCKS_SECRET_KEY || "",
   });
   const session = await getServerSession(authOptions);
-  const email = session?.user?.email || '';
+  const email = session?.user?.email || "";
   if (email) {
     const roomId = uniqid.time();
     return await liveblocksClient.createRoom(roomId, {
       defaultAccesses: [],
       usersAccesses: {
-        [email]: ['room:write'],
+        [email]: ["room:write"],
       },
       metadata: {
         boardName: name,
@@ -29,30 +28,30 @@ export async function createBoard(name: string) : Promise<false | RoomInfo> {
   return false;
 }
 
-export async function addEmailToBoard(boardId:string, email:string) {
+export async function addEmailToBoard(boardId: string, email: string) {
   const room = await liveblocksClient.getRoom(boardId);
   const usersAccesses = room.usersAccesses;
-  usersAccesses[email] = ['room:write'];
+  usersAccesses[email] = ["room:write"];
   console.log(usersAccesses);
-  await liveblocksClient.updateRoom(boardId, {usersAccesses});
+  await liveblocksClient.updateRoom(boardId, { usersAccesses });
   return true;
 }
 
-export async function updateBoard(boardId:string, updateData:any) {
+export async function updateBoard(boardId: string, updateData: any) {
   const result = await liveblocksClient.updateRoom(boardId, updateData);
-  console.log({result});
+  console.log({ result });
   return true;
 }
 
-export async function removeEmailFromBoard(boardId:string, email:string) {
+export async function removeEmailFromBoard(boardId: string, email: string) {
   const room = await liveblocksClient.getRoom(boardId);
-  const usersAccesses:any = room.usersAccesses;
+  const usersAccesses: any = room.usersAccesses;
   usersAccesses[email] = null;
-  await liveblocksClient.updateRoom(boardId, {usersAccesses});
+  await liveblocksClient.updateRoom(boardId, { usersAccesses });
   return true;
 }
 
-export async function deleteBoard(boardId:string) {
+export async function deleteBoard(boardId: string) {
   await liveblocksClient.deleteRoom(boardId);
   return true;
 }
--- src/components/views/LoginView.tsx
+++ src/components/views/LoginView.tsx
@@ -1,12 +1,57 @@
-'use client';
-import {signIn} from "next-auth/react";
+"use client";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Poppins } from "next/font/google";
+import localfont from "next/font/local";
+
+const headingFont = localfont({
+  src: "../../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function LoginView() {
   return (
-    <div className="w-full pt-8 text-center">
-      <button
-        onClick={() => signIn('google')}
-        className="primary">Login</button>
+    <div className="flex items-center justify-center flex-col py-36">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
+        <h1 className="text-4xl md:text-6xl text-center text-neutral-800 mb-6">
+          The all-in-one solution for
+        </h1>
+        <div
+          className="text-3xl md:text-6xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white px-4 p-2 rounded-md py-4
+        w-fit"
+        >
+          Team Collaboration
+        </div>
+      </div>
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className
+        )}
+      >
+        Tasknest helps teams organize their work,
+        <br /> collaborate on projects, and stay on track
+      </div>
+      <div className="w-full text-center">
+        <button
+          onClick={() => signIn("google")}
+          className={cn(
+            "bg-black text-white mt-8 rounded-md py-2 px-4",
+            textFont.className
+          )}
+        >
+          Get TaskNest for Free!
+        </button>
+      </div>
     </div>
   );
 }
